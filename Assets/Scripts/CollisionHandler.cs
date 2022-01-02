ad6e565d719a8693ffa4d917428789ad8ff78290
nguyenkduy <nguyenkduywork@gmail.com>
2022-01-02 16:19:58 +0100

    Added 1 enemy + animation timeline

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"{this.name} TRIGGERED BY {other.gameObject.name}");
+    }
+}
