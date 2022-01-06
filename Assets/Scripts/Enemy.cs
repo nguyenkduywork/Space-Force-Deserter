dc8b0a629d90272de0602340e637530814f5ec64
nguyenkduy <nguyenkduywork@gmail.com>
2022-01-06 22:22:26 +0100

    Update enemy script (rigid body no longer needed, added FindWithTag() for the parent folder to avoid using SerializeField

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,11 +9,11 @@ public class Enemy : MonoBehaviour
     private ScoreBoard scoreBoard;
     private int playerDps;
     private MeshRenderer mesh;
-    private BoxCollider collider;
+    private BoxCollider thiscollider;
+    GameObject parentGameObject;
 
     [SerializeField] private GameObject deathVFX;
     [Header("This is to tidy up explosion vfx in the hierarchy")]
-    [SerializeField] private Transform parent;
 
     [Header("Score earned when being hit with a particle")]
     [SerializeField] private int scorePerHit = 1;
@@ -27,19 +27,25 @@ public class Enemy : MonoBehaviour
     
     private void Start()
     {
+        //Note to self: should not use FindObjectOfType - slow function, not advised to use multiple times (in update() etc)
         playerDps = FindObjectOfType<PlayerControls>().getDPS();
         scoreBoard = FindObjectOfType<ScoreBoard>();
-        AddRigidBody();
+        //FindWithTag() finds the object with that tag
+        parentGameObject = GameObject.FindWithTag("SpawnRuntime");
+        
+        //AddRigidBody();
         audioSource = GetComponent<AudioSource>();
         mesh = GetComponent<MeshRenderer>();
-        collider = GetComponent<BoxCollider>();
+        thiscollider = GetComponent<BoxCollider>();
     }
-
+    
+    /*
     private void AddRigidBody()
     {
         gameObject.AddComponent<Rigidbody>();
         GetComponent<Rigidbody>().useGravity = false;
     }
+    */
 
 
     void OnParticleCollision(GameObject other)
@@ -57,17 +63,19 @@ public class Enemy : MonoBehaviour
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         //move vfx objects inside a parent folder to clear up the hierarchy 
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         mesh.enabled = false;
-        collider.enabled = false;
+        thiscollider.enabled = false;
 
     }
-
+    
+    //Destroy enemy GameObject
     void DestroyEnemy()
     {
         Destroy(gameObject);
     }
-
+    
+    //Each time a laser hit the enemy, enemy loses HP equals to the amount of player DPS
     private void processHits()
     {
         HP -= playerDps;
